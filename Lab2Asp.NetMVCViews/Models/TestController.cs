a1807d45d17a9e21ef17074e0f84afef8be55954
jebs01 <jebs01@gmail.com>
2017-04-01 14:27:19 +1100

    Lab2

---
+++ Lab2Asp.NetMVCViews/Models/TestController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2Asp.NetMVCViews.Models
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetView()
+        {
+            //return View("MyView");
+
+            if (false)
+            {
+                return View("MyView"); }
+            else
+            {
+               // return View("Fork");
+
+            return Content ("helloo........");
+            }
+        }
+    }
+}
